894b87139934b207301ff4407813d11be0c1cf42
Martin Burger <mburger@kochag.ch>
2022-10-04 15:44:13 +0200

    initial commit

---
+++ U_PiCalc_HS2022/main.c
@@ -0,0 +1,82 @@
+/*
+ * U_PiCalc_HS2022.c
+ *
+ * Created: 20.03.2018 18:32:07
+ * Author : -
+ */ 
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include "avr_compiler.h"
+#include "pmic_driver.h"
+#include "TC_driver.h"
+#include "clksys_driver.h"
+#include "sleepConfig.h"
+#include "port_driver.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "event_groups.h"
+#include "stack_macros.h"
+
+#include "mem_check.h"
+
+#include "init.h"
+#include "utils.h"
+#include "errorHandler.h"
+#include "NHD0420Driver.h"
+
+#include "ButtonHandler.h"
+
+
+void controllerTask(void* pvParameters);
+
+int main(void)
+{
+	vInitClock();
+	vInitDisplay();
+	
+	xTaskCreate( controllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 3, NULL);
+
+	vDisplayClear();
+	vDisplayWriteStringAtPos(0,0,"PI-Calc HS2022");
+	
+	vTaskStartScheduler();
+	return 0;
+}
+
+void controllerTask(void* pvParameters) {
+	initButtons();
+	for(;;) {
+		updateButtons();
+		if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
+			char pistring[12];
+			sprintf(&pistring[0], "PI: %.8f", M_PI);
+			vDisplayWriteStringAtPos(1,0, "%s", pistring);
+		}
+		if(getButtonPress(BUTTON2) == SHORT_PRESSED) {
+			
+		}
+		if(getButtonPress(BUTTON3) == SHORT_PRESSED) {
+			
+		}
+		if(getButtonPress(BUTTON4) == SHORT_PRESSED) {
+			
+		}
+		if(getButtonPress(BUTTON1) == LONG_PRESSED) {
+			
+		}
+		if(getButtonPress(BUTTON2) == LONG_PRESSED) {
+			
+		}
+		if(getButtonPress(BUTTON3) == LONG_PRESSED) {
+			
+		}
+		if(getButtonPress(BUTTON4) == LONG_PRESSED) {
+			
+		}
+		vTaskDelay(10/portTICK_RATE_MS);
+	}
+}
